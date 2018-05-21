a46d6a549ee1a559afaa69170a9b56d44a11d9a0
Russ Watson <russ@bakerstreet.industries>
2018-05-21 06:01:48 -0500

    No longer print out password arguments within service calls.

--- src/auth/handler.ts
+++ src/auth/handler.ts
@@ -1,4 +1,21 @@
 import "reflect-metadata";
+import { setDefault, IHookProperties } from "ts-log-class";
+import { Logger } from "../utils/logging";
+
+setDefault({
+  hook: (logProps: IHookProperties) => {
+    if (logProps.arguments.hasOwnProperty("user")) {
+      const user: any = JSON.parse(logProps.arguments.user);
+      user.password = "[secret]";
+      logProps.arguments.user = JSON.stringify(user);
+    }
+    return JSON.stringify(logProps);
+  },
+  out: (message?: any, ...optionalParameters: any[]) => {
+    Logger.debug(message, optionalParameters);
+  }
+});
+
 import { wrapFunction } from "../utils/wrapper";
 import * as main from "./main";
 
